5de88c95de2cfe8d4d37f766d9657ced1fcb42a2
NabeelMustafa <nabeelmustafa729@gmail.com>
2020-01-25 18:52:47 +0500

    Container1 chnages

--- src/components/container01.js
+++ src/components/container01.js
@@ -26,60 +26,20 @@ import { Container, Row, Col } from 'reactstrap';
 class Container01 extends React.Component {
     render() {
       return (
-      <div id="container01" className="full screen" data-scroll-id="one" data-scroll-behavior="center" style={{justifyContent: 'center'}} /*className={container01Styles.columns} className={container01Styles.full} className={container01Styles.full.screen}*/>
-          
-          
-          <div /*className={container01Styles.inner} style={{justifyContent: 'center'}}*/>
-            {/**<div className={container01Styles.afterSpacer}>**/}
-                
-              {/*<h2 id="text01" className={container01Styles.style1}>Feed The People</h2>*/}
-              {/**<hr id="divider03" />**/}
-              {/*<p id="text02" className={container01Styles.style2}>Morbi semper nisl et dictum cursus. In hac habitasse platea dictumst. </p>*/}
-              {/**<ResponsivePlayer01 />**/}
-              {/*<ReactWebMediaPlayer
-	            width={500} height={350}
-	            title="Chemtrails"
-	            thumbnail={Chemtrails}
-	            audio={Chemtrailsmp3}
-	            logo={{
-		            img: "../../images/logo-white.png",
-		            href: "#"
-	                }}
-                />*/}
-              <Mediaplayer01a />
-           
-            <Container style={{
-                      paddingBottom: '10em',
-                      justifyContent: 'center',
-          }}>
-          
-
-          <Row /*style={{justifyContent: 'center'}}*/>
-              <Col xs="6">
-              </Col>
-              <Col xs="6">
-              <SpotifyPlayer
-                uri="spotify:album:6NwNaCmHzQNvTSi0hRpALe"
-                size={{width: 376, height: 400}}
-                theme='black'
-              />
-              {/*<SpotifyPlayer
-                uri="spotify:track:7sxyTA1EIoeEENTkMHQPmd"
-                size={{width: 376, height: 400}}
-                theme='black'
-              />*/}
-              </Col>
-              
-            </Row>
-          </Container>
-              <br />
-              <hr />
-              {/*<Rmusic2 />*/}
-              
-
-              
-          </div>
-          </div>
+      <div 
+      id="container01" className="full screen" data-scroll-id="one" data-scroll-behavior="center"  >
+        <Row>
+          <Col style={{textAlign: "center"}}>
+            <SpotifyPlayer />
+          </Col>
+          <Col>
+            <TContentCards01/>
+          </Col>
+          <Col>
+            <TContentCards02/>
+          </Col>
+        </Row>
+      </div>
       )
     }
 }
