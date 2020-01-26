bebf5b26c18fdc628533cb48024cfc6c91a541c7
NabeelMustafa <nabeelmustafa729@gmail.com>
2020-01-26 22:22:46 +0500

    footer and some other stuff

--- src/components/container04.js
+++ src/components/container04.js
@@ -5,41 +5,47 @@ import Albumcover from '../images/album-cover.jpg'
 //import TContentCards from './tcontentcardsrow1'
 import ResponsivePlayer from './responsiveplayer01'
 import formStyles from '../css/forms.module.css'
+import { Container, Row, Col } from 'reactstrap';
 
 
 
 class Container04 extends React.Component {
     render() {
       return (
-        <div id="container04" data-scroll-id="signup" data-scroll-behavior="center" className="container columns full screen">
-        <div className={container04Styles.inner}>
-          <div>
-            <h2 id="text11" className={container04Styles.style1}>Contact</h2>
-           {/** <hr id="divider05" />**/}
-            <p id="text13" className={container04Styles.style2}>Morbi semper nisl et dictum cursus. In hac habitasse platea dictumst. Aliquam blandit etiam vel massa eget mollis. Donec at quam orci. Proin et semper metus consequat etiam.</p>
-          </div>
-          <div style={{textAlign: "center", marginTop: '1rem', marginBottom: '1rem'}}>>
-            <form id="form02" method="post">
-              <div className={container04Styles.inner}>
-                <div className={formStyles.field}>
-                  <label htmlFor="form02-fname">Name</label>
-                  <input type="text" name="fname" id="form02-fname" placeholder maxLength={128} required />
+        <div id="container04" data-scroll-id="signup" data-scroll-behavior="center" className="container columns full screen" >
+        
+            <div style={{textAlign: "center", marginTop: '1rem', marginBottom: '1rem', width: '350px'}}>
+            <br/>
+            <br/>
+            <br/>
+            <h1 id="text11" className={container04Styles.style1}  style={{color: "white", fontSize: '3rem'}}>Contact Us</h1>
+            <br/>
+            <br/>
+              <form id="form02" method="post">
+                <div className={container04Styles.inner}>
+                  <div className={formStyles.field}>
+                    <label htmlFor="form02-fname">Name</label>
+                    <input type="text" name="fname" id="form02-fname" placeholder maxLength={128} required />
+                  </div>
+                  <div className={formStyles.field}>
+                    <label htmlFor="form02-email">Email</label>
+                    <input type="email" name="email" id="form02-email" placeholder maxLength={128} required />
+                  </div>
+                  <div className={formStyles.field}>
+                    <input type="text" name="sms" id="form02-sms" placeholder="Sms" maxLength={128} />
+                  </div>
+                  <br/>
+                  <div >
+                    <button type="submit" style={{width: '100%', padding: '0px', margin: '0px'}}>Submit</button>
+                  </div>
                 </div>
-                <div className={formStyles.field}>
-                  <label htmlFor="form02-email">Email</label>
-                  <input type="email" name="email" id="form02-email" placeholder maxLength={128} required />
-                </div>
-                <div className={formStyles.field}>
-                  <input type="text" name="sms" id="form02-sms" placeholder="Sms" maxLength={128} />
-                </div>
-                <div className={formStyles.actions}>
-                  <button type="submit">Submit</button>
-                </div>
-              </div>
-              <input type="hidden" name="id" defaultValue="form02" />
+                <input type="hidden" name="id" defaultValue="form02" />
+              </form>
+            </div>
+            <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+              <input type="text" name="fname" />
+              <input type="email" name="email" />
             </form>
-          </div>
-        </div>
       </div>
       )
     }
--- src/components/tcontentcardsrow1.js
+++ src/components/tcontentcardsrow1.js
@@ -10,15 +10,540 @@ import {  Row, Col } from 'reactstrap';
 import Modal from 'react-awesome-modal';
 
 
+const lyrics = [{
+    name: 'KEMTRAILS',
+    lyrics: {
+        p1: `Hay hay
+        That’s a big old plane
+        White lines in the sky,
+        Making me wonder 
+        
+        Hay hay
+        Take the planes away
+        They dirty the sky
+        Making it Hotter
+         
+        Big O cans flying through the sky
+        Sure know why it’s on my mind
+        We talk about a better way
+        All the lines all through the sky
+         
+        When it all comes down to fait 
+        There’s nothing there that’s good for you`,
+        p2: `
+        They say the lines goanna make it rain  
+        It’s only a matter of time no lie
+        Cheam trails 
+        Protecting us from bad sun rays
+        Others say it’s such a haze
+        Carbon dioxide from the planes
+        Soot particulates are to blame When it all comes down to fait 
+        There’s nothing there that’s good for you                                        
+        They say the lines goanna make it rain  
+        It’s only a matter of time no lie
+
+        Cheam trails
+        Cheam trails
+        Cheam trails
+
+        `
+    }
+},{
+    name: 'FEED the PEOPLE',
+    lyrics: {
+        p1: `INTRO LEAD /      
+        Voice -- Hayeeeeee
+        Feed the people inside your life 
+        Feed the people just get in line 
+        Feed the people another lie
+        Feed the people open wide
+        
+        Uuuu uou 
+        Every time we try to get ahead
+        Feed the People
+        Feed the people
+        Uuuu uou 
+        We give you take it’s all that we can stand 
+        Feed the People
+        Feed the People
+        Uuuu uou 
+        
+        Feed the people almighty dollar
+        Feed the people ain’t worth a dime
+        Feed the people whole pack of lies
+        Feed the people just one more time
+        Uuuu uou 
+        Every time we try to get ahead
+        Feed the People
+        Feed the people
+        Uuuu uou 
+
+        We give you take it’s more than we can stand 
+        Feed the People
+        Feed the People
+        Uuuuuou
+        `,
+        p2: `
+        Feed the people insecticides
+        Feed the people the pesticides 
+        Feed the people your dirty water
+        Feed the people there hungry
+
+        Uuuu uou 
+        Every time we try to get ahead
+        Feed the People
+        Feed the people
+
+        Uuuu uou 
+        Feed the people insecticides
+        Feed the people the pesticides 
+        Feed the people your dirty water
+        Feed the people we’re hungry
+        LEAD / EFFECTS
+        END
+
+        `
+    }
+},{
+    name: 'BURN',
+    lyrics: {
+        p1: `Hey Hey Hey 
+
+        Burn it
+        Burn it
+        Burn it to the ground      
+        
+        Ashes -blowing in the wind
+        Smoke -burns through your eye’s
+        Dust -all through the air
+        Darkness -fall’s through the sky
+        
+        Burn it   - Look around           
+        Burn it   - It’s falling down     
+        Burn it.   -To the ground             
+        Burn it   -We will all be 
+        Falling down`,
+        p2: `
+        Burn it    -Plumes of smoke
+        Burn it     -Until we choke
+        Burn it    -There Nothing left
+        Burn it.   -We will burn it to the ground
+
+        LEAD  
+
+        We will burn it to the ground
+        We will burn it to the ground
+
+        END
+
+        `
+    }
+},{
+    name: 'A-THING',
+    lyrics: {
+        p1: `You get a piece of me 
+        ya taste so fine 
+        Yes I like to hold you down
+        
+        As you move I turn        
+        Right on time 
+        O so fine
+        
+        Shhhhhh
+        
+        Don’t say a word 
+        Harder never deep enough
+        Shiver with delight
+        Rocking it through the night
+        
+        Don’t say a word 
+        Harder never deep enough
+        Shiver with delight
+        Rocking it through the night
+
+        It’s not what we know 
+        It’s who we know
+        We thought it was good
+        So here’s what I got to say
+
+        Oooooo
+        `,
+        p2: `
+        Try to confuse me                 
+        In this state of mine
+        Try to abuse me
+        So easy you defy
+
+        Try to change my world
+        I blink you lie
+        Yes same o rumor in town
+        As you move on out
+        Right on time
+        O not again
+
+        Shhhhhh
+
+        Don’t say a word
+        Hammer beating in my 
+        Shiver to the bone
+        Were always good in bed
+
+        Another thing I must confess
+        You’re still a taker who never gives
+        This won’t happen
+        Ever again
+
+        Always a lie
+        You always deny
+
+
+
+
+
+
+
+
+        Try to confuse me                         
+        In this state of mine
+        Try to abuse me
+        So easily defy
+
+        You’re a taker who never gives back
+        Difficult and exhausting 
+        I’m over it.......... 
+
+        `
+    }
+},{
+    name: 'HATE',
+    lyrics: {
+        p1: `Hayyyyyyyyyy
+
+        Hate is my best friend
+        Hate is what I feel for you
+        Fear is what you see in me
+        Pain I have in store for you
+        
+        Leave me alone
+        Let me be
+        Let me go
+        
+        Is this what you want me to see
+        Is this what you wanted me to be
+        Look at what you’ve done to me
+        What are you doing to me
+        I want you to leave me alone
+        
+        Haaaaa
+
+        Hate is my best friend
+        Hate is my best friend who’s lonely
+        I hate to be the only one
+        Don’t you know that I’m living in hell 
+
+        Is this what you wanted to see
+        Is this what you want me to be
+`,
+        p2: `
+        I don’t understand 
+        Why don’t you let me out 
+
+        Ask your mother while you’re freaking me out 
+        I’ll find a way out to cure this disease
+        I’ve never seen the man on the moon 
+
+        ha ha ha ha
+
+        I hate you
+        Leave me alone 
+        I hate that you hate
+        What we want to be 
+        Is this what you wanted to see
+        Is this what you want me to be
+
+        LEAVE ME ALONE
+
+        You’re alone in the dark, 
+        ha ha ha ha ha
+
+        `
+    }
+},{
+    name: 'SORRY',
+    lyrics: {
+        p1: `Intro.
+        Cry Vocals 
+        Uuummm aaaaaaa
+        
+        I’m so sorry
+        Ya I didn’t mean
+        to make you  go away
+        
+        Air—
+        
+        I’m so sorry 
+        
+        For putting you ten feet 
+        Under ground
+        
+        Sorry 
+        Didn’t mean to make you go
+
+        Down into the ground 
+        I put you there`,
+        p2: `
+        Since you’ve gone away 
+        I miss your soul
+
+        Sorry
+        Down into the ground
+
+        Sorry
+        I didn’t mean to let you go   
+
+        LEAD—
+
+        Mmmmmmmm
+        The grass is always greener
+        Where I know you are 
+        `
+    }
+},{
+    name: 'CHAINS',
+    lyrics: {
+        p1: `Chains
+        Chains
+        
+        Nogoooo
+        
+        Bang my head a way I don’t 
+        want to be 
+        Don’t know why you take me there
+        Twist and turn my insecurity 
+        Do you like to watch me     squirm  
+        
+        Shut up 
+        Shut up and leave me be
+        There’s no pleasing you
+        All the pain I grieve 
+        Why do I endure your shame
+        Breaking through - through all these chains
+        Remove the shackles of my pain
+        Breaking through-through all these chains
+
+        All the chains
+        What you do is insanity                    
+        Insane to me
+
+        The fear inside me
+        Is suspicious               
+        Suspicious 
+
+        Are you pleased with your 
+        devise        
+
+        Devise
+`,
+        p2: `
+        Devise
+
+        Or will you leave 
+        As I lay dying              
+
+        Shut up 
+        Shut up and leave me be
+        There’s no pleasing you
+        All the pain I’ve grieve  
+        Nogooo
+
+        LEAD
+
+        All the pain I greave 
+        Breaking through the chains
+
+        Why do I endure your shame
+        Breaking through - through all these chains
+        Remove the shackles of my pain
+        Bbreaking through - through all  these chains
+
+
+
+
+
+        All the chains          
+        All the Pain
+        All the chains           
+        All the Pain—
+        All the chains 
+        All the chains
+        All the chains 
+        All the chains   
+
+        Nogoooo
+
+        All the pain I grieve
+        Breaking through the chains
+        All the pain I grieve
+
+        Breaking through the chains
+        Chains 
+        Chains 
+        Chains
+        Pain I greave   
+        Chains
+        Through the chains
+
+        `
+    }
+},{
+    name: 'EARTH',
+    lyrics: {
+        p1: `Who am I                  
+        Who brings you light
+        Through the darkness
+        Of the night   
+        
+        Oooooo  
+        
+        Who am I     
+        What are we
+        Where do we go    
+        Why do we live
+        How much longer
+        For the eeeeeeearth
+        
+        Who am I          
+        
+        Who brings you light
+        Through the darkness
+        Of the night
+
+        Aaaaaa Aaaaaa Aaaa
+
+        WE WILL RETURN   
+
+         `,
+        p2: `
+
+        LEAD
+
+        Through the night
+        Black or white
+        Makes no difference
+        Without the light  
+
+        Soooooooo
+
+        Who am I   
+        What are we
+        Where do we go   
+        Why do we live
+        How much longer     
+        For the eeeeearth
+
+        On the earth 
+        Do the good 
+        Aand good will be
+        Until the end of end
+
+        Aaaaaa Aaaaaa Aaaa
+
+        HuuRrrraaaaaaaaaa
+
+        `
+    }
+},{
+    name: 'NEVER',
+    lyrics: {
+        p1: `I’m not an angel 
+        I never said I was
+        Some things are best                 left unsaid
+        Halfway through my life  
+        I’m still searching on
+        For something better than yesterday 
+        
+        Mmmmmmmmm
+        
+        Pathway through our lives
+        Youth were searching for
+        Never knowing what you’ll      find
+        
+        If love could’ve saved us you’d still be here
+        You’re the life of the party Uninvited  
+        Stop preaching to the preacher we’ve all been around the block
+        It’s not our first rodeo              
+        how far can we go
+
+        Red Yellow Green Blue are the Colors of our soles 
+
+
+            `,
+        p2: `
+        Life lives in memories from      
+        the sunrise to the sun set  
+        Dose it matter after all we’re do we go from here
+        Can you really see - see where you’re going too
+        
+        Walking to the edge of the earth to the top of the ice 
+        As it starts to crack slipping I’m falling down 
+        Better way of life 
+        In my life I’m nobody 
+        Cause I can’t spend it with you
+
+        What goes around comes around as it reaches through your soul
+
+        What you’ve done will come back to you
+        `
+    }
+},{
+    name: 'COLORS',
+    lyrics: {
+        p1: `Hay hay
+        That’s a big old plane
+        White lines in the sky,
+        Making me wonder 
+        
+        Hay hay
+        Take the planes away
+        They dirty the sky
+        Making it Hotter
+         
+        Big O cans flying through the sky
+        Sure know why it’s on my mind
+        We talk about a better way
+        All the lines all through the sky
+         
+        When it all comes down to fait 
+        There’s nothing there that’s good for you`,
+        p2: `
+        They say the lines goanna make it rain  
+        It’s only a matter of time no lie
+        Cheam trails 
+        Protecting us from bad sun rays
+        Others say it’s such a haze
+        Carbon dioxide from the planes
+        Soot particulates are to blame When it all comes down to fait 
+        There’s nothing there that’s good for you                                        
+        They say the lines goanna make it rain  
+        It’s only a matter of time no lie
+
+        Cheam trails
+        Cheam trails
+        Cheam trails
+
+        `
+    }
+}]
 
 class TContentCards01 extends React.Component {
+    
 
     constructor() {
         super();
      
         this.state = {
           modalIsOpen: false,
-          content: ''
+          id: 0
         };
     }
 
@@ -27,6 +552,7 @@ class TContentCards01 extends React.Component {
     }
 
     render() {
+        console.log(lyrics[0]);
         let {
           href,
           thumbnail,
@@ -38,9 +564,19 @@ class TContentCards01 extends React.Component {
         return (
             <div>
                 <Modal
-                    visible={this.state.modalIsOpen} width="400" height="300" effect="fadeInUp" onClickAway={() => this.closeModal()}
+                    visible={this.state.modalIsOpen} width="600" height="400" effect="fadeInUp" onClickAway={() => this.closeModal()}
                     >
-                    <h1> {this.state.content} </h1>
+                    <Row style={{display: 'flex'}}>
+                        <Col xs="6" lg="6" md="6" style={{margin: "1rem"}}><h1> 
+                        
+                        {lyrics[this.state.id].name} </h1></Col>
+                    </Row>
+
+                    <Row style={{display: 'flex'}}>
+                        <Col xs="6" lg="6" md="6" style={{margin: "1rem"}}><p> {lyrics[this.state.id].lyrics.p1} </p></Col>
+                        <Col xs="6" lg="6" md="6" style={{margin: "1rem"}}><p> {lyrics[this.state.id].lyrics.p2} </p></Col>
+                    </Row>
+                    
                 </Modal>
                 <Row style={{display: 'flex'}}>
                     <Col xs="2" lg="2" md="6">
@@ -54,7 +590,7 @@ class TContentCards01 extends React.Component {
                             '',
                             ''
                         ]}
-                        onClick={ () => { this.setState({ modalIsOpen: true, content: 'Chem Trails Lyrics'})} }
+                        onClick={ () => { this.setState({ modalIsOpen: true, id: 0})} }
                     />                
                     </Col>
                     <Col xs="2" lg="2" md="6">
@@ -68,7 +604,7 @@ class TContentCards01 extends React.Component {
                             '',
                             ''
                         ]}
-                        onClick={ () => { this.setState({ modalIsOpen: true, content: 'Feed the people lyrics'})} }
+                        onClick={ () => { this.setState({ modalIsOpen: true, id: 1})} }
                         
 
                     />                
@@ -83,7 +619,7 @@ class TContentCards01 extends React.Component {
                             '',
                             ''
                         ]}
-                        onClick={ () => { this.setState({ modalIsOpen: true, content: 'Burn Lyrics'})} }
+                        onClick={ () => { this.setState({ modalIsOpen: true, id: 2})} }
                         
                     />                
                     </Col> <Col xs="2" lg="2" md="6">
@@ -97,7 +633,7 @@ class TContentCards01 extends React.Component {
                             '',
                             '',
                         ]}
-                        onClick={ () => { this.setState({ modalIsOpen: true, content: 'Another Lyrics'})} }
+                        onClick={ () => { this.setState({ modalIsOpen: true, id: 3})} }
                         
                     />                
                     </Col>
@@ -112,7 +648,7 @@ class TContentCards01 extends React.Component {
                             '',
                             '',
                         ]}
-                        onClick={ () => { this.setState({ modalIsOpen: true, content: 'Hate Lyrics'})} }
+                        onClick={ () => { this.setState({ modalIsOpen: true, id: 4})} }
                         
                     />                
                     </Col>
