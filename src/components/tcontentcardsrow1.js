994b7568a0c185ea141de263aab614afa2e49f80
salemaziel <mymainemail0501@gmail.com>
2020-01-31 01:19:20 -0800

    added starwarscrawler for rolling lyrics text test

--- src/components/tcontentcardsrow1.js
+++ src/components/tcontentcardsrow1.js
@@ -13,6 +13,10 @@ import { FaPlay } from 'react-icons/fa'
 import {  Row, Col } from 'reactstrap';
 import Modal from 'react-awesome-modal';
 
+import Crawl from 'react-star-wars-crawl'
+import 'react-star-wars-crawl/lib/index.css'
+import Lyrics from './lyrics'
+import '../css/tcontencardsrow1.css'
 
 const song = [
     {
@@ -67,6 +71,15 @@ const song = [
 
     }
 ]
+
+//const Lyriccrawl = () => (
+//    <Crawl
+//     title=''
+//      text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+//      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur
+//      "
+//    />
+//)
 class TContentCards01 extends React.Component {
 
 
@@ -112,7 +125,9 @@ class TContentCards01 extends React.Component {
                     effect="fadeInUp"
                     onClickAway={() => this.closeModal()}
                 >
+                    
                     <Row style={{ display: 'flex'}}>
+                    
                         <div style={{
                             backgroundImage: `url(${this.state.id})` ,
                             height: "600px", 
@@ -120,6 +135,8 @@ class TContentCards01 extends React.Component {
                             backgroundSize: 'cover', 
                             backgroundPosition: 'center center',
                             } }>
+                            {/*<Lyriccrawl>*/}
+                            <Lyrics />
                             <div style={{ 
                                 position: 'absolute',
                                 left: '85%',
@@ -140,9 +157,11 @@ class TContentCards01 extends React.Component {
 
                                 />
                             </div>
-
+                            {/*</Lyriccrawl>*/}
                         </div>
+                        
                     </Row>
+                    
 
                 </Modal>
                 <Row style={{display: 'flex'}}>
