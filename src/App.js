8f86cd30ca536db38400812786626aea34cc110f
salemaziel <mymainemail0501@gmail.com>
2020-02-18 16:08:27 -0800

    rsdhgd

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
  
 import Home from './Home';
-//import Discography from './components/discography';
+import Discography from './components/discography';
 //import './App.css'
 //import './css/main2.css'
  
@@ -13,7 +13,7 @@ class App extends Component {
         <div>
             <Switch>
              <Route path="/" component={Home} exact/>
-             {/*<Route path="/discography" component={Discography}/>*/}
+             <Route path="/discography" component={Discography}/>
             <Route component={Error}/>
            </Switch>
         </div> 
