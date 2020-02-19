6eebca420e5b3887b15404ec8caaba4e6852fc4a
salemaziel <mymainemail0501@gmail.com>
2020-02-18 18:23:28 -0800

    added title to container02

--- src/components/container02.js
+++ src/components/container02.js
@@ -31,16 +31,16 @@ class Container02 extends React.Component {
         <div 
       id="container02" className="container default full screen" data-scroll-id="one" data-scroll-behavior="center">
         
-       {/* <Row className="songheaderRow" style={{
+        <Row className="songheaderRow" style={{
             display: "flex",
             justifyContent: "center",
             textAlign: "center",
             position: 'relative',
-            /*top: "2em",* /
+            /*top: "2em",*/
             
         }}> 
           <Col>
-              <img className="songheader" src={Logo} alt='' /*mode='fit' * / style={{
+              <img className="songheader" src={Logo} alt='' /*mode='fit' */ style={{
                                                                             marginTop: "0",
               }} />
               <p 
@@ -57,7 +57,7 @@ class Container02 extends React.Component {
              > Feed The People</p>
               </Col>
            
-              </Row>*/}
+              </Row>
         <Row /*style={{  display: /*'flex',* /
           
           }}*/>
