604c1f180ea4e5fe9489eea866d71abacf4e0a33
salemaziel <mymainemail0501@gmail.com>
2020-02-27 18:16:34 -0800

    gvkjv

--- src/components/containerBio.js
+++ src/components/containerBio.js
@@ -2,16 +2,19 @@ import React from 'react'
 import '../css/containerBio.css'
 import Jackbio from '../images/JacksCave_1600x.jpg'
 import Lazyload from 'react-lazyload'
+import Jcavemobile from './jcavemobile'
+
 
 const ContainerBio = () => (
         <div id="containerBio" data-scroll-id="three" data-scroll-behavior="center" className="container columns full screen">
             <div id="inner" >
                 <div className="afterSpacer">
                   
-                    <img src={Jackbio} 
+                    <img className="jcavefull" src={Jackbio} 
                           alt="Primary artist of Chaotic Order Jack Dubord"
                           className="jackbio">
                     </img>
+                    <Jcavemobile className="jcavemobile" />
                   
             
           </div>
